61771d6b62b6ae58b2a78d129442a71b2cea3219
Phill Conrad <pconrad@cs.ucsb.edu>
2026-05-09 12:56:40 -0700

    pc - update url in systemInfo fixtures to be appropriate for this repo

--- frontend/src/fixtures/systemInfoFixtures.js
+++ frontend/src/fixtures/systemInfoFixtures.js
@@ -3,25 +3,25 @@ const systemInfoFixtures = {
     springH2ConsoleEnabled: true,
     showSwaggerUILink: true,
     oauthLogin: "/oauth2/authorization/google",
-    sourceRepo: "https://github.com/ucsb-cs156-s26/STARTER-team02",
+    sourceRepo: "https://github.com/ucsb-cs156/proj-scaffold",
   },
   showingNeither: {
     springH2ConsoleEnabled: false,
     showSwaggerUILink: false,
     oauthLogin: "/oauth2/authorization/google",
-    sourceRepo: "https://github.com/ucsb-cs156-s26/STARTER-team02",
+    sourceRepo: "https://github.com/ucsb-cs156/proj-scaffold",
   },
   oauthLoginUndefined: {
     springH2ConsoleEnabled: false,
     showSwaggerUILink: false,
-    sourceRepo: "https://github.com/ucsb-cs156-s26/STARTER-team02",
+    sourceRepo: "https://github.com/ucsb-cs156/proj-scaffold",
   },
   initialData: {
     initialData: true,
     springH2ConsoleEnabled: true,
     showSwaggerUILink: true,
     oauthLogin: "/oauth2/authorization/google",
-    sourceRepo: "https://github.com/ucsb-cs156-s26/STARTER-team02",
+    sourceRepo: "https://github.com/ucsb-cs156/proj-scaffold",
   },
 };
 
