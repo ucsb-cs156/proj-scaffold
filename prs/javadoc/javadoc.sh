6481f5debabb8e2beec79884f356ca5ec82a7a46
pconrad <pconrad@cs.ucsb.edu>
2026-09-23 22:18:44 +0000

    Deploying to gh-pages from @ ucsb-cs156/proj-scaffold@c6b28c6748b42c811320cf785bc2faec7d05727c 🚀

--- prs/javadoc/javadoc.sh
+++ prs/javadoc/javadoc.sh
@@ -1,1 +1,1 @@
-/opt/hostedtoolcache/Java_Oracle_jdk/21/x64/bin/javadoc -J-Duser.language= -J-Duser.country= @options @packages
+/opt/hostedtoolcache/Java_Oracle_jdk/25/x64/bin/javadoc -J-Duser.language= -J-Duser.country= @options @packages
